ac5484bd14e416a7228db28f67d08b09ca1e4fb2
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-02-11 23:12:42 +0700

    DepedencyInjection

---
+++ tests/Feature/ServiceContainerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Data\Foo;
+
+class ServiceContainerTest extends TestCase
+{
+    public function testDepedencyInjection()
+    {
+        $foo1 = $this->app->make(Foo::class);
+        $foo2 = $this->app->make(Foo::class);
+
+        self::assertEquals('Foo', $foo1->foo());
+        self::assertEquals('Foo', $foo2->foo());
+        self::assertNotSame($foo1, $foo2);
+    }
+}
