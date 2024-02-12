0e355d6a4d2d4d418bb0f158ccf1014a5e5d52b5
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-02-13 00:09:35 +0700

    test binding, test Interface

--- tests/Feature/DependencyInjectionTest.php
+++ tests/Feature/DependencyInjectionTest.php
@@ -5,13 +5,18 @@ namespace Tests\Feature;
 use App\Data\Bar;
 use App\Data\Foo;
 use App\Data\Person;
+use App\Services\HelloService;
+use App\Services\HelloServiceIndonesia;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\App;
 use Tests\TestCase;
+
+use function PHPUnit\Framework\assertNotSame;
 
 class DependencyInjectionTest extends TestCase
 {
-    public function testDependencyInjection()
+    public function testDependency()
     {
         $foo = new Foo();
         $bar = new Bar($foo);
@@ -60,5 +65,35 @@ class DependencyInjectionTest extends TestCase
         self::assertEquals("Jimmy", $person1->firstname);
         self::assertEquals("Jimmy", $person2->firstname);
         self::assertSame($person1, $person2);
+    }
+
+    public function testDepedencyInjection()
+    {
+        $this->app->singleton(Foo::class, function ($app) {
+            return new Foo();
+        });
+
+
+        $this->app->singleton(Bar::class, function ($app) {
+            $foo = $app->make(Foo::class);
+            return new Bar($foo);
+        });
+
+        $foo = $this->app->make(Foo::class);
+        $bar1 = $this->app->make(Bar::class);
+        $bar2 = $this->app->make(Bar::class);
+
+        self::assertSame($foo, $bar1->foo);
+
+        self::assertSame($bar1, $bar2);
+    }
+
+    public function testInterfaceToClass()
+    {
+        $this->app->singleton(HelloService::class, HelloServiceIndonesia::class);
+
+        $helloService = $this->app->make(HelloService::class);
+
+        self::assertEquals('Halo Jimmy', $helloService->hello('Jimmy'));
     }
 }
