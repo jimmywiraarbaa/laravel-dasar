87cc3d65d728396091cd10dea251546b0a2d5047
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-02-12 23:10:18 +0700

    test binding, test singleton

--- tests/Feature/DependencyInjectionTest.php
+++ tests/Feature/DependencyInjectionTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Data\Bar;
 use App\Data\Foo;
+use App\Data\Person;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -16,5 +17,33 @@ class DependencyInjectionTest extends TestCase
         $bar = new Bar($foo);
 
         self::assertEquals("Foo and bar", $bar->bar());
+    }
+
+    public function testBind()
+    {
+        $this->app->bind(Person::class, function ($app) {
+            return new Person("Jimmy", "Jimmy");
+        });
+
+        $person1 = $this->app->make(Person::class);
+        $person2 = $this->app->make(Person::class);
+
+        self::assertEquals("Jimmy", $person1->firstname);
+        self::assertEquals("Jimmy", $person2->lastname);
+        self::assertNotSame($person1, $person2);
+    }
+
+    public function testSingelton()
+    {
+        $this->app->singleton(Person::class, function ($app) {
+            return new Person("Jimmy", "Jimmy");
+        });
+
+        $person1 = $this->app->make(Person::class);
+        $person2 = $this->app->make(Person::class);
+
+        self::assertEquals("Jimmy", $person1->firstname);
+        self::assertEquals("Jimmy", $person2->lastname);
+        self::assertSame($person1, $person2);
     }
 }
