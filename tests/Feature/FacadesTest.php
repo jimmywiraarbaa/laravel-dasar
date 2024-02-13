6e4bda8756f16ddbbfc971f2fab85ccc7142a267
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-02-13 10:30:18 +0700

    Facades

---
+++ tests/Feature/FacadesTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Config;
+use Tests\TestCase;
+
+class FacadesTest extends TestCase
+{
+    public function testConfig()
+    {
+        $config = $this->app->make('config');
+        $firstName3 = $config->get('contoh.author.first');
+
+        $firstName1 = config('contoh.author.first');
+        $firstName2 = Config::get('contoh.author.first');
+
+        self::assertEquals($firstName1, $firstName2);
+        self::assertEquals($firstName1, $firstName3);
+    }
+}
