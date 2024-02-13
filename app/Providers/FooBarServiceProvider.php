6be270e9f8d45c820a021525008f30b7251b6521
bangbangnam <69713060+jimmywiraarbaa@users.noreply.github.com>
2024-02-13 10:06:52 +0700

    Depedency Service Provider

--- app/Providers/FooBarServiceProvider.php
+++ app/Providers/FooBarServiceProvider.php
@@ -6,9 +6,10 @@ use App\Data\Bar;
 use App\Data\Foo;
 use App\Services\HelloService;
 use App\Services\HelloServiceIndonesia;
+use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
 
-class FooBarServiceProvider extends ServiceProvider
+class FooBarServiceProvider extends ServiceProvider implements DeferrableProvider
 {
     public array $singletons = [
         HelloService::class => HelloServiceIndonesia::class
@@ -19,6 +20,7 @@ class FooBarServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        // echo "FooBarServiceProvider";
         $this->app->singleton(Foo::class, function () {
             return new Foo();
         });
@@ -34,5 +36,10 @@ class FooBarServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+    }
+
+    public function provides()
+    {
+        return [HelloService::class, Foo::class, Bar::class];
     }
 }
